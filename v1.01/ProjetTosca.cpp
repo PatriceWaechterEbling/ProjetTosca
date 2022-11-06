9b8d764721c4bc679d354e15ca65b803d86e9c86
Patrice André Waechter-Ebling <97856916+PatriceWaechterEbling@users.noreply.github.com>
2022-11-06 03:33:39 -0500

    ProjetTosca v1.0.1
    nouvelle edition de línterface EndUser vis a de ProjetTosca v1.0

---
+++ v1.01/ProjetTosca.cpp
@@ -0,0 +1,374 @@
+
+#include <windows.h>
+#include <exdisp.h>
+#include <exdispid.h>
+#include <commctrl.h>
+#pragma comment(lib,"Comctl32")
+// Définir les messages pour notre application:
+#define BEFORENAVIGATE2       WM_USER
+#define DOWNLOADBEGIN         WM_USER+1
+#define DOWNLOADCOMPLETE      WM_USER+2
+#define NAVIGATECOMPLETE2     WM_USER+3
+#define DOCUMENTCOMPLETE      WM_USER+4
+#define COMMANDSTATECHANGE    WM_USER+5
+
+#pragma warning(disable:26495)
+#pragma warning(disable:6054)
+#pragma warning(disable:6387)
+#pragma warning(disable:6031)
+#pragma warning(disable:8012)
+#pragma warning(disable:28251)
+#pragma warning(disable:9035)
+#pragma warning(disable:4616)
+#pragma warning(disable:4244)
+#pragma warning(disable:4312)
+
+class Evenem : public IDispatch
+{
+	private:
+	long ref;
+	HWND fenetre;
+	BSTR url;
+
+	public:
+		Evenem(HWND fenet){fenetre=fenet;}
+		~Evenem(){SysFreeString(url);}
+		STDMETHODIMP QueryInterface(REFIID iid, void ** ppvObject){	if (iid==IID_IUnknown || iid==IID_IDispatch || iid==DIID_DWebBrowserEvents2){*ppvObject=this;AddRef(); return S_OK;} else return E_NOINTERFACE;	}
+		ULONG STDMETHODCALLTYPE AddRef()	{ 	return InterlockedIncrement(&ref);  	}
+		ULONG STDMETHODCALLTYPE Release(){	int tmp = InterlockedDecrement(&ref);	if (tmp==0) delete this; 	return tmp;	}
+		HRESULT STDMETHODCALLTYPE GetTypeInfoCount(unsigned int FAR* pctinfo){return E_NOTIMPL;}
+		HRESULT STDMETHODCALLTYPE GetTypeInfo(unsigned int iTInfo, LCID  lcid, ITypeInfo FAR* FAR*  ppTInfo){return E_NOTIMPL;}
+		HRESULT STDMETHODCALLTYPE GetIDsOfNames(REFIID riid, OLECHAR FAR* FAR* rgszNames, unsigned int cNames, LCID lcid, DISPID FAR* rgDispId){ return E_NOTIMPL; }
+		HRESULT STDMETHODCALLTYPE Invoke(DISPID dispIdMember, REFIID riid, LCID lcid, WORD wFlags, DISPPARAMS FAR* pDispParams, VARIANT FAR* parResult, EXCEPINFO FAR* pExcepInfo, unsigned int FAR* puArgErr)	{
+			IUnknown *pIUnk;
+			VARIANT  *vurl ;
+			if (!pDispParams) return E_INVALIDARG;  
+			switch (dispIdMember) {
+				case DISPID_BEFORENAVIGATE2:{pIUnk = pDispParams->rgvarg[6].pdispVal;SendMessage(fenetre, BEFORENAVIGATE2, (WPARAM)pIUnk, 0);}break;
+				case DISPID_DOWNLOADBEGIN: {SendMessage(fenetre,DOWNLOADBEGIN,0,0);}break;
+				case DISPID_DOWNLOADCOMPLETE:{SendMessage(fenetre,DOWNLOADCOMPLETE,0,0);}break;
+				case DISPID_NAVIGATECOMPLETE2: {
+					pIUnk=pDispParams->rgvarg[1].pdispVal;
+					vurl= pDispParams->rgvarg[0].pvarVal;
+					url = vurl->bstrVal;
+					SendMessage(fenetre,NAVIGATECOMPLETE2,(WPARAM)pIUnk,(LPARAM)url);
+				}break;
+				case DISPID_DOCUMENTCOMPLETE:{pIUnk=pDispParams->rgvarg[1].pdispVal;SendMessage(fenetre,DOCUMENTCOMPLETE,(WPARAM)pIUnk,0);}break;
+				case DISPID_COMMANDSTATECHANGE: {
+					long command =pDispParams->rgvarg[1].lVal;
+					VARIANT_BOOL etat=pDispParams->rgvarg[0].boolVal;
+					SendMessage(fenetre,COMMANDSTATECHANGE,(WPARAM)command,(LPARAM)etat);
+				}break;
+				case DISPID_NEWWINDOW2:{pDispParams->rgvarg[0].pvarVal->vt = VT_BOOL;pDispParams->rgvarg[0].pvarVal->boolVal = VARIANT_TRUE;}break;
+				default:
+					break;
+				}	
+		  return S_OK;
+			}
+};
+IWebBrowser2   *pIWeb;
+WNDPROC OldEditProc;
+HWND hConteneur,hAdresse;
+WNDCLASS wc;
+
+INT MsgBox(HWND hDlg, CHAR* lpszText, CHAR* lpszCaption, DWORD dwStyle, INT lpszIcon) { //MessageBox personalise
+	MSGBOXPARAMS lpmbp{};
+	lpmbp.hInstance = wc.hInstance;
+	lpmbp.cbSize = sizeof(MSGBOXPARAMS);
+	lpmbp.hwndOwner = hDlg;
+	lpmbp.dwLanguageId = MAKELANGID(0x0800, 0x0800); //par defaut celui du systeme
+	lpmbp.lpszText = lpszText;
+	lpmbp.lpszCaption = lpszCaption;
+	lpmbp.dwStyle = dwStyle | 0x00000080L;
+	if (lpszIcon == 0) {
+		lpmbp.lpszIcon = (LPCSTR)101;
+	}
+	else {
+		lpmbp.lpszIcon = MAKEINTRESOURCE(lpszIcon);
+	}
+	lpmbp.lpfnMsgBoxCallback = 0;
+	return  MessageBoxIndirect(&lpmbp);
+}
+LRESULT CALLBACK ProcedureSousClassementEdit(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam){
+	switch (message){
+	case WM_CHAR: if (wParam == VK_RETURN) return 0;break;
+	case WM_KEYDOWN:
+		if (wParam == VK_RETURN){
+			WCHAR url[256];
+			char buff[256];
+			GetWindowText(hAdresse,buff,256);
+			MultiByteToWideChar (CP_ACP, 0,buff, -1, url, 256);
+			pIWeb->Navigate(url,0,0,0,0);
+			return 0;
+		}
+		break;
+	default:
+		break;
+	}
+	return CallWindowProc(OldEditProc, hwnd, message, wParam, lParam);
+}
+
+LRESULT CALLBACK WndProc( HWND hWnd, UINT messg, WPARAM wParam, LPARAM lParam )
+{
+	static HWND hPrecedente,hSuivante,hArreter,hActualiser,hDemarrage,hEnregistrer,hImprimer,hAller;
+	static HWND hCadre1,hCadre2,hTadresse,hEtat;
+	static int PageCounter=0;
+	static int ObjCounter=0;
+	char tampon[256];
+	static BSTR titre;
+	WCHAR url[256];
+
+	switch(messg)
+	{
+	case WM_CREATE: {
+		RECT rc;
+		GetClientRect(hWnd, &rc);
+		TBADDBITMAP tbab{};
+		const int nbBoutons = 10;
+		TBBUTTON tbb[nbBoutons];
+		char txtBoutons[nbBoutons][15] = { "Précédente", "Suivante", "Arrêter", "Actualiser", "Maison", "Enregistrer","Imprimer" };
+		hPrecedente = CreateWindow("BUTTON", "", WS_CHILD | WS_VISIBLE, 5, 6, 90, 20, hWnd, 0, 0, 0);
+		hSuivante = CreateWindow("BUTTON", "", WS_CHILD | WS_VISIBLE, 105, 6, 90, 20, hWnd, 0, 0, 0);
+		hArreter = CreateWindow("BUTTON", "", WS_CHILD | WS_VISIBLE, 205, 6, 90, 20, hWnd, 0, 0, 0);
+		hActualiser = CreateWindow("BUTTON", "", WS_CHILD | WS_VISIBLE, 305, 6, 90, 20, hWnd, 0, 0, 0);
+		hDemarrage = CreateWindow("BUTTON", "", WS_CHILD | WS_VISIBLE, 405, 6, 90, 20, hWnd, 0, 0, 0);
+		hEnregistrer = CreateWindow("BUTTON", "", WS_CHILD | WS_VISIBLE, 505, 6, 90, 20, hWnd, 0, 0, 0);
+		hImprimer = CreateWindow("BUTTON", "", WS_CHILD | WS_VISIBLE, 605, 6, 90, 20, hWnd, 0, 0, 0);
+		hTadresse = CreateWindow("STATIC", "Adresse  :", WS_CHILD | WS_VISIBLE | SS_CENTER, 7, 35, 90, 20, hWnd, 0, 0, 0);
+		hAller = CreateWindow("BUTTON", "Aller", WS_CHILD | WS_VISIBLE, 610, 34, 80, 20, hWnd, 0, 0, 0);
+		hCadre1 = CreateWindow("BUTTON", 0, WS_CHILD | WS_VISIBLE | BS_GROUPBOX, 05, 22, 690, 36, hWnd, 0, 0, 0);
+		HIMAGELIST hImageList = ImageList_Create(16, 16, ILC_COLOR24 | ILC_MASK, 99, 0);
+		for (int x = 0; x < nbBoutons; x++) {
+			ImageList_AddIcon(hImageList, LoadIcon(wc.hInstance, (LPCSTR)200 + x));
+		}
+		HWND hToolBar = CreateWindow(TOOLBARCLASSNAME, NULL, WS_CHILD | WS_VISIBLE, 0, 0, 0, 0, hWnd, (HMENU)0x1386, wc.hInstance, NULL);
+		SendMessage(hToolBar, TB_BUTTONSTRUCTSIZE, (WPARAM)sizeof(TBBUTTON), 0);
+		SendMessage(hToolBar, TB_SETIMAGELIST, 0, (LPARAM)hImageList);
+		SendMessage(hToolBar, (UINT)TB_SETHOTIMAGELIST, 0, (LPARAM)hImageList);
+		SendMessage(hToolBar, TB_AUTOSIZE, 0, 0);
+		tbab.hInst = wc.hInstance;
+		tbab.nID = IDB_STD_SMALL_COLOR;
+		SendMessage(hToolBar, TB_ADDBITMAP, 0, (LPARAM)&tbab);
+		ZeroMemory(tbb, sizeof(tbb));
+		for (int x = 0; x < 10; x++) {
+			tbb[x].iBitmap = x;
+			tbb[x].fsState = TBSTATE_ENABLED;
+			tbb[x].fsStyle = BTNS_CHECK | BTNS_SHOWTEXT /*| BTNS_AUTOSIZE*/ ;
+			if (!(tbb[x + 1].fsStyle = 0x01)) {
+				tbb[x].idCommand = 2000 + x;
+				tbb[x].iString = x;
+			}
+		}
+		SendMessage(hToolBar, TB_ADDBUTTONS, 5, (LPARAM)&tbb);
+		// Création de tous les contrôles:
+		hAdresse = CreateWindowEx(WS_EX_CLIENTEDGE, "EDIT", 0, WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL, rc.right-590, 15, 490, 20, hWnd, 0, 0, 0);
+		// Obtenir l'adresse de la procédure de l'EDIT de la barre d'adresse pour le sous-classer:
+		hEtat = CreateStatusWindow(WS_CHILD | WS_VISIBLE, "ProjetTosca", hWnd, 6000);
+		OldEditProc = (WNDPROC)SetWindowLong(hAdresse, GWLP_WNDPROC, (LPARAM)ProcedureSousClassementEdit);
+		// Griser tous les boutons:
+		EnableWindow(hPrecedente, 0);
+		EnableWindow(hSuivante, 0);
+		EnableWindow(hArreter, 0);
+		EnableWindow(hActualiser, 0);
+		EnableWindow(hEnregistrer, 0);
+		EnableWindow(hImprimer, 0);
+		EnableWindow(hAller, 0);
+	}break;
+		
+		case WM_COMMAND:
+			GetWindowText(hAdresse,tampon,256);
+			EnableWindow(hAller,lstrlen(tampon));
+
+			if( (HWND)lParam == hPrecedente ) // Clic sur "Précédente"
+			{
+				// Aller à la page précédente:
+				pIWeb->GoBack();
+				break;
+			}
+			if( (HWND)lParam == hSuivante ) // Clic sur "Suivante"
+			{
+				// Aller à la page suivante:
+				pIWeb->GoForward();
+				break;
+			}
+			if( (HWND)lParam == hArreter ) // Clic sur "Arrêter"
+			{
+				// Initialiser les compteurs:
+				PageCounter=ObjCounter=0;
+				// Arrêter la navigation:
+				pIWeb->Stop();
+				// Griser le bouton "Arrêter" et dégriser "Actualiser"
+				EnableWindow(hArreter,0);
+				EnableWindow(hActualiser,1);
+				SetWindowText(hEtat,"Arrêté");
+				break;
+			}
+			if((HWND) lParam == hActualiser ) // Clic sur "Actualiser"
+			{
+				// Actualier la page:
+				pIWeb->Refresh2(0);
+				break;
+			}
+			if( (HWND)lParam == hDemarrage ) // Clic sur "Démarrage"
+			{
+				// Lancer la page de démarrage:
+							MultiByteToWideChar (CP_ACP, 0,"https://services.csvdc.qc.ca/toscanetcrif/asp/Tosca.aspx", -1, url, 256);
+			// Lancer la navigation:
+			pIWeb->Navigate(url,0,0,0,0);
+
+			//	pIWeb->GoHome();
+				break;
+			}
+			if((HWND) lParam == hEnregistrer ) // Clic sur "Enregistrer"
+			{
+				// Enregister la page:
+				pIWeb->ExecWB(OLECMDID_SAVEAS,OLECMDEXECOPT_DODEFAULT, 0,0);
+				break;
+			}
+			if( (HWND)lParam == hImprimer ) //Clic sur "Imprimer"
+			{
+				// Imprimer la page:
+				pIWeb->ExecWB(OLECMDID_PRINT,OLECMDEXECOPT_DONTPROMPTUSER, 0,0);
+				break;
+			}
+
+			if((HWND) lParam == hAller ) // Clic sur "Aller"
+			{
+			// Récupérer et convertir le lien de la barre d'adresse:
+			WCHAR url[256];
+			GetWindowText(hAdresse,tampon,256);
+			MultiByteToWideChar (CP_ACP, 0,tampon, -1, url, 256);
+			// Lancer la navigation:
+			pIWeb->Navigate(url,0,0,0,0);
+			}
+			break;
+
+		case BEFORENAVIGATE2: {
+			if ((IUnknown*)wParam==pIWeb)	PageCounter=ObjCounter=0;
+			PageCounter++;
+			EnableWindow(hArreter,1);
+			EnableWindow(hActualiser,0);
+			EnableWindow(hEnregistrer,0);
+			EnableWindow(hImprimer,0);
+		}break;
+
+		case DOWNLOADBEGIN: {
+			ObjCounter++;
+			EnableWindow(hArreter,1);
+			EnableWindow(hActualiser,0);
+			EnableWindow(hEnregistrer,0);
+			EnableWindow(hImprimer,0);
+			SetWindowText(hEtat,"Navigation");
+		}break;
+		case DOWNLOADCOMPLETE: {
+			ObjCounter--;
+			if (PageCounter==0 && ObjCounter==0)
+			{
+				EnableWindow(hArreter,0);
+				EnableWindow(hActualiser,1);
+				EnableWindow(hEnregistrer,1);
+				EnableWindow(hImprimer,1);
+				SetWindowText(hEtat,"Terminé");
+			}
+		}break;
+		case DOCUMENTCOMPLETE: {
+			PageCounter--;
+			if (PageCounter==0){	
+				pIWeb->get_LocationName(&titre);
+				WideCharToMultiByte(CP_ACP,0,titre,-1,tampon,256,0,0);
+				lstrcat(tampon," - ProjetTosca");
+				SetWindowText(hWnd,tampon);
+				EnableWindow(hArreter,0);
+				EnableWindow(hActualiser,1);
+				EnableWindow(hEnregistrer,1);
+				EnableWindow(hImprimer,1);
+				SetWindowText(hEtat,"Terminé");
+			}
+		}break;
+		case NAVIGATECOMPLETE2: {
+			if((IUnknown*)wParam==pIWeb){
+				WideCharToMultiByte(CP_ACP,0,(LPCWSTR)lParam,-1,tampon,256,0,0);
+				SetWindowText(hAdresse,tampon);
+				pIWeb->get_LocationName(&titre);
+				WideCharToMultiByte(CP_ACP,0,titre,-1,tampon,256,0,0);
+				lstrcat(tampon," - ProjetTosca");
+				SetWindowText(hWnd,tampon);
+			}
+		}break;
+		case COMMANDSTATECHANGE: {
+			if (wParam==2) EnableWindow(hPrecedente,(BOOL)lParam);
+			if (wParam==1) EnableWindow(hSuivante,(BOOL)lParam);
+		}break;
+		case WM_SIZE:{
+			RECT rc;
+			MoveWindow(hConteneur,5,62,LOWORD(lParam)-8, HIWORD(lParam)-100,1);
+			GetClientRect(hConteneur,&rc);
+			MoveWindow(hEtat,0,rc.bottom-32,rc.right, 25,1);
+		}return 0;
+		case WM_CTLCOLORDLG:return (LONG)(HBRUSH)CreateSolidBrush(RGB(0, 0, 128));
+		case WM_CTLCOLORSTATIC:SetTextColor((HDC)wParam, RGB(255, 0, 255)); SetBkMode((HDC)wParam, TRANSPARENT); return (LONG)wc.hbrBackground;
+		case WM_CTLCOLOREDIT: SetTextColor((HDC)wParam, RGB(0xA0, 0xFF, 0xAA)); SetBkMode((HDC)wParam, TRANSPARENT); return (LONG)(HBRUSH)CreateSolidBrush(RGB(0, 0, 128));
+		case WM_CTLCOLORLISTBOX:SetTextColor((HDC)wParam, RGB(0, 255, 255)); SetBkMode((HDC)wParam, TRANSPARENT); return (LONG)(HBRUSH)CreateSolidBrush(RGB(0, 128, 0));
+		case WM_CTLCOLORBTN:SetTextColor((HDC)wParam, RGB(0, 255, 255)); SetBkMode((HDC)wParam, TRANSPARENT); return (LONG)(HBRUSH)CreateSolidBrush(RGB(128, 0, 0));
+		case WM_CTLCOLORMSGBOX: return (long)(HBRUSH)CreateSolidBrush(RGB(255, 0, 128));
+		case WM_CLOSE: {
+			SysFreeString(titre);
+			DestroyWindow(hWnd);
+		}break;
+		case WM_DESTROY: {PostQuitMessage( 0 );	}break;
+		default:return( DefWindowProc( hWnd, messg, wParam, lParam ) );
+	}
+	return 0;
+}
+int WINAPI WinMain( HINSTANCE hInst,HINSTANCE hPreInst,LPSTR lpszCmdLine, int nCmdShow ){
+	char NomClasse[]   = "ProjetTosca";
+	wc.lpszClassName 	= NomClasse;
+	wc.hInstance 		= hInst;
+	wc.lpfnWndProc		= WndProc;
+	wc.hCursor			= LoadCursor( NULL, IDC_ARROW );
+	wc.hIcon			= LoadIcon( wc.hInstance,(LPCSTR) 101 );
+	wc.lpszMenuName	    = NULL;
+	wc.hbrBackground	= GetSysColorBrush(COLOR_BTNFACE);;
+	wc.style			= 0;
+	wc.cbClsExtra		= 0;
+	wc.cbWndExtra		= 0;
+	if (!RegisterClass(&wc)) return 0;
+	RECT rc;
+	GetClientRect(GetDesktopWindow(), &rc);
+	HWND hWnd = CreateWindow( NomClasse,"ProjetTosca", WS_POPUP |WS_BORDER | WS_SYSMENU|WS_CAPTION,rc.left,rc.top,rc.right,rc.bottom, 0, 0, hInst,0);
+	ShowWindow(hWnd, nCmdShow );
+	UpdateWindow( hWnd );
+	HINSTANCE hDLL = LoadLibrary("atl.dll");
+	typedef HRESULT (WINAPI *PAttachControl)(IUnknown*, HWND,IUnknown**);
+	PAttachControl AtlAxAttachControl = (PAttachControl) GetProcAddress(hDLL, "AtlAxAttachControl");
+	RECT rect;
+	GetClientRect(hWnd,&rect);
+	hConteneur=CreateWindowEx(WS_EX_CLIENTEDGE,"BUTTON","",WS_CHILD | WS_VISIBLE,5,62,rect.right-8,rect.bottom-67,hWnd,0,0,0);
+	CoInitialize(0);
+	CoCreateInstance(CLSID_WebBrowser,0,CLSCTX_ALL,IID_IWebBrowser2,(void**)&pIWeb);
+	AtlAxAttachControl(pIWeb,hConteneur,NULL);
+	IConnectionPointContainer* pCPContainer=NULL;
+    pIWeb->QueryInterface(IID_IConnectionPointContainer,(void**)&pCPContainer);
+	IConnectionPoint *pConnectionPoint;
+	pCPContainer->FindConnectionPoint(DIID_DWebBrowserEvents2, &pConnectionPoint);
+	Evenem *pEvnm;
+	pEvnm= new Evenem(hWnd);
+	DWORD dwCookie = 0;
+	pConnectionPoint->Advise(pEvnm, &dwCookie);
+	if (pCPContainer) pCPContainer->Release();
+		WCHAR url[256];
+						MultiByteToWideChar (CP_ACP, 0,"https://services.csvdc.qc.ca/toscanetcrif/asp/Tosca.aspx", -1, url, 256);
+			pIWeb->Navigate(url,0,0,0,0);
+	MSG Msg;
+	while( GetMessage( &Msg, 0, 0, 0 ) ){
+		TranslateMessage( &Msg );
+		DispatchMessage( &Msg );
+	}
+	pConnectionPoint->Unadvise(dwCookie);
+   pConnectionPoint->Release();
+	delete pEvnm;
+	pIWeb->Release();
+    CoUninitialize();		
+	FreeLibrary(hDLL);
+	return( Msg.wParam);
+}
